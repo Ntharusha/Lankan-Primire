fdf85c7c948e0bd95da466899e1a6391752f9b41
Ntharusha <ntb069@gmail.com>
2026-04-29 12:03:28 +0530

    fix backend api

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,5 +1,7 @@
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_API_BASE_URL || 'http://13.201.141.85:5000');
+const socket = io(window.location.origin, {
+  path: '/socket.io'
+});
 
 export default socket;
