0deb54db830fb98792dd9e9ac57a6e9ccd5ad8de
Ntharusha <ntb069@gmail.com>
2026-05-01 08:59:10 +0530

    Revert to port 5000 for AWS firewall compatibility

--- server/index.js
+++ server/index.js
@@ -182,7 +182,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
